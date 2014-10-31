e5353b8de444224e7f3997b5d571b8a785a1a0d8
Mitesh Shah <djtesh@gmail.com>
2014-10-31 13:19:15 -0400

    - Changed button push animation. - Added App Icon. - Tweaked the layout and look.

--- One800Game/DigitButton.swift
+++ One800Game/DigitButton.swift
@@ -12,5 +12,42 @@ import UIKit
 class DigitButton : UIButton {
 
     var digit : Int = 0;
+    var fill : UIImageView?;
+    
+    override init() {
+        super.init()
+        setup()
+        
+        
+    }
+
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+    
+    func setup() {
+        var theImage = UIImage(named: "Fill.png")
+      fill = UIImageView(image: theImage)
+        fill?.frame.size = self.frame.size
+        fill?.alpha = 0
+//        fill?.tintColor = UIColor(white: 0.5, alpha: 1.0)
+      addSubview(fill!)
+        
+        addTarget(self, action: "pulse", forControlEvents: UIControlEvents.TouchUpInside)
+    
+    }
+    
+    func pulse()
+    {
+        
+        fill?.alpha = 0.75
+        UIView.animateWithDuration(0.5, animations: {
+            self.fill!.alpha = 0
+        })
+        
+    }
+    
+    
     
 }
